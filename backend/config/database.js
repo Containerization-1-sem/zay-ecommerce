c2c081785d7abb9feac87ecedbc4207afa19c6b5
Bast38900 <bast38900@edu.ucl.dk>
2024-05-15 13:29:56 +0200

    add pg to json, and add gb to backend + productadd example

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,7 @@
 module.exports = {
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "zay",
-  //host: process.env.DB_HOSTNAME || "localhost",
-  host: 'db',
-  dialect: "mysql"
+  username: process.env.DB_USERNAME || "youruser",
+  password: process.env.DB_PASSWORD || "yourpassword",
+  database: process.env.DB_DATABASE || "yourdatabase",
+  host: process.env.DB_HOSTNAME || "db",
+  dialect: "postgres"
 }
